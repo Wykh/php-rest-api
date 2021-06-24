af7dee43c12ef8c05d558c3d67cf76a6115e9e2a
Wythuk <bobcopy555@gmail.com>
2021-06-25 02:35:23 +0500

    created carckase of project

--- web/public/news.php
+++ web/public/news.php
@@ -5,15 +5,14 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Инвестиции</title>
+    <title>News</title>
 </head>
 <body>
-<ul>
-    <li> <a href="news.php"> Новости </a> </li>
-    <li> Обучение </li>
-    <li> Личный кабинет </li>
-    <li> Поддержка </li>
-</ul>
+
+ВОТ ЭТО НОВОСТИ!
 
 </body>
 </html>
+
+<?php
+?>
